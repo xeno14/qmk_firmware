883614081cec1949fe95f5b1b178108c3a8a9683
xeno14 <integral14.dev@gmail.com>
2020-07-13 10:45:30 +0900

    tab shift

--- keyboards/helix/pico/keymaps/xeno14/keymap.c
+++ keyboards/helix/pico/keymaps/xeno14/keymap.c
@@ -58,6 +58,7 @@ enum macro_keycodes {
 #define CTLTAB      LCTL(KC_TAB)
 #define CTLSPC      LCTL(KC_SPC)
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
@@ -66,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  \   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Tab  |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  '   |
+   * |Shift |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  '   |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
    * |Adjust| GUI  | Alt  |AltCtl|Shift |Lower |Space |Enter |Raise | Bksp |   =  |   `  |   [  |  ]   |
    * `-------------------------------------------------------------------------------------------------'
@@ -75,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS, \
-      KC_TAB, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
       ADJUST,  KC_LGUI, KC_LALT, LALTCTL, KC_LSFT, LOWER, KC_SPC,  KC_ENT,  RAISE, KC_BSPC, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC  \
       ),
   /* Colemak
@@ -116,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | Del  |
+   * | Tab  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |CtlTab|  F1  |  F2  |  F3  |  F4  |  F5  |             |  F11 |   _  |   +  |   {  |   }  |  |   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -126,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+      KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
       CTLTAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, _______, KC_LBRC, KC_RBRC, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, KC_LT,   KC_GT,   _______ \
@@ -134,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |CtlSpc|
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |CtlSpc|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |CtlTab|      | Home | PgDn | PgUp | End  |             | Left | Down |  Up  |Right |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -144,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    CTLSPC,  \
+      KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    CTLSPC,  \
       CTLTAB,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,                    KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______, _______, \
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, CTLSPC, _______, _______, KC_DEL,  _______, _______, _______, _______ \
