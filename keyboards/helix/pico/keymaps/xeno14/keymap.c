fce4c234f64970aac0b71f1d7e069099c1f1801f
xeno14 <integral14.dev@gmail.com>
2020-07-14 09:41:35 +0900

    swap alt and altctl

--- keyboards/helix/pico/keymaps/xeno14/keymap.c
+++ keyboards/helix/pico/keymaps/xeno14/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  '   |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| GUI  | Alt  |AltCtl|Shift |Lower |Space |Enter |Raise | Bksp |   =  |   `  |   [  |  ]   |
+   * |Adjust| GUI  |AltCtl| Alt  |Shift |Lower |Space |Enter |Raise | Bksp |   =  |   `  |   [  |  ]   |
    * `-------------------------------------------------------------------------------------------------'
    */
 
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-      ADJUST,  KC_LGUI, KC_LALT, LALTCTL, KC_LSFT, LOWER, KC_SPC,  KC_ENT,  RAISE, KC_BSPC, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC  \
+      ADJUST,  KC_LGUI, LALTCTL, KC_LALT, KC_LSFT, LOWER, KC_SPC,  KC_ENT,  RAISE, KC_BSPC, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC  \
       ),
   /* Colemak
    * ,-----------------------------------------.             ,-----------------------------------------.
