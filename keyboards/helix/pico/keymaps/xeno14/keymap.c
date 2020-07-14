b4e5a6185f3cc432785d6be58fcf34ffd9661a01
xeno14 <integral14.dev@gmail.com>
2020-07-14 22:17:53 +0900

    adjust symbol locations

--- keyboards/helix/pico/keymaps/xeno14/keymap.c
+++ keyboards/helix/pico/keymaps/xeno14/keymap.c
@@ -70,21 +70,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |ESC/~ |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  -   |
+   * |ESC/~ |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |Ctl/TB|   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  \   |
+   * |Ctl/TB|   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |Shift |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  '   |
+   * |Shift |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |  `   |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| GUI  |AltCtl| Alt  |Sft/TB|Lower |Space |Enter |Raise | Bksp |   =  |   `  |   [  |  ]   |
+   * |Adjust| GUI  |AltCtl| Alt  |Sft/TB|Lower |Space |Enter |Raise | Bksp |   =  |   -  |   [  |  ]   |
    * `-------------------------------------------------------------------------------------------------'
    */
 
 [_QWERTY] = LAYOUT( \
-      KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-      MCTLTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-      ADJUST,  KC_LGUI, LALTCTL, KC_LALT, MSFTTAB, LOWER, KC_SPC,  KC_ENT,  RAISE, KC_BSPC, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC  \
+      KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+      MCTLTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV, \
+      ADJUST,  KC_LGUI, LALTCTL, KC_LALT, MSFTTAB, LOWER, KC_SPC,  KC_ENT,  RAISE, KC_BSPC, KC_EQL,  KC_MINS, KC_LBRC, KC_RBRC  \
       ),
   /* Colemak
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -124,18 +124,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Tab  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | Del  |
+   * | Tab  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  |   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |CtlTab|  F1  |  F2  |  F3  |  F4  |  F5  |             |  F11 |   _  |   +  |   {  |   }  |  |   |
+   * |CtlTab|  F1  |  F2  |  F3  |  F4  |  F5  |             |  F11 |   _  |   +  |   {  |   }  |  "   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F6  |  F7  |  F8  |  F9  |  F10 |             |  F12 |   "  |      |   [  |   ]  |  "   |
+   * |      |  F6  |  F7  |  F8  |  F9  |  F10 |             |  F12 |   "  |      |   [  |   ]  |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      | Del  |      |   <  |   >  |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-      CTLTAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+      KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+      CTLTAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQT, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,                    KC_F12,  KC_DQT,  _______, KC_LBRC, KC_RBRC, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, KC_LT,   KC_GT,   _______ \
       ),
